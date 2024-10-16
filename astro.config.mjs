c13023e4f6e83e56caa40b6e1683d27d23eb5d6d
caclm10 <lewinxander@gmail.com>
2024-10-16 17:36:56 +0700

    fix: Base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://caclm10.github.io',
+    base: '/matcha-latte',
     integrations: [tailwind()]
 });
